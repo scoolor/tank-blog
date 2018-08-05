e14ea7c942cffb7bee45c62cf8bb60ff5aa86584
liuhao <liu546hao@163.com>
2018-08-05 17:07:49 +0800

    layout

---
+++ public/admin/admin/src/components/Layout.vue
@@ -0,0 +1,23 @@
+<template>
+  <section class="container" >
+    <section class="menu" id="menu">
+    </section>
+    <section class="content-container" id="content-container">
+    </section>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Layout'
+}
+</script>
+
+<style scoped>
+  #menu {
+    background-color: black;
+  }
+  #content-container {
+    background-color:green;
+  }
+</style>
--- public/admin/admin/src/router/index.js
+++ public/admin/admin/src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Layout from '@/components/Layout'
 import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
@@ -8,6 +9,11 @@ export default new Router({
   routes: [
     {
       path: '/',
+      name: 'Layout',
+      component: Layout
+    },
+    {
+      path: '/tutorial',
       name: 'HelloWorld',
       component: HelloWorld
     }
