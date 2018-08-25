21b16fb46fb889921abb316ecbd7412351604288
liuhao <liu546hao@163.com>
2018-08-25 20:22:27 +0800

    添加pipeline单元测试

--- tests/engine/pipeline/HttpPipeLineTest.php
+++ tests/engine/pipeline/HttpPipeLineTest.php
@@ -9,9 +9,18 @@
 namespace tests\engine\pipeline;
 
 
+use engine\pipeline\HttpPipeLine;
 use tests\BaseTestCase;
 
 class HttpPipeLineTest extends BaseTestCase
 {
+    public function testSend()
+    {
+        $request = null;
+        $httpPipe = new HttpPipeLine();
 
+        $result = $httpPipe->send($request);
+
+        $this->assertInstanceOf('engine\pipeline\HttpPipeLine', $result);
+    }
 }
