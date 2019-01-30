d674a2ea8862d1af53fed235c12f579bc0bc7fa3
liuhao <liu546hao@163.com>
2019-01-30 16:36:53 +0800

    some edit

--- public/app/src/components/Header.vue
+++ public/app/src/components/Header.vue
@@ -1,28 +1,34 @@
 <template>
   <el-row>
     <el-col :span="4">
-      <div>
-        Logo
-      </div>
+      <ul class="nav">
+        <li class="nav-list">hello</li>
+      </ul>
     </el-col>
-    <el-col :span="2" class="el-col-offset-16">
-      <div>
-        Category
-      </div>
-    </el-col>
-    <el-col :span="2">
-      <div>
-        Action
-      </div>
+    <el-col :span="4">
+      <ul class="nav">
+        <li class="nav-list">world</li>
+        <li class="nav-list">good</li>
+      </ul>
     </el-col>
   </el-row>
 </template>
 
 <script>
 export default {
-  name: 'header.vue'
+  name: 'Header'
 }
 </script>
 
 <style scoped>
+  div {
+    width: 200px;
+    margin : auto 0;
+  }
+  .nav {
+    list-style-type: none;
+  }
+  .nav-list {
+    float : left;
+  }
 </style>
