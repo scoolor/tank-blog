80ba0a6905b64a6c6b28b6365f8c5d63b547de61
liuhao <liu546hao@163.com>
2019-02-01 18:26:46 +0800

    some commit

--- public/app/src/components/MyContent.vue
+++ public/app/src/components/MyContent.vue
@@ -1,7 +1,16 @@
 <template>
   <div style="width : 66%; margin : 50px auto;">
-    <div style="width : 100%; height : 60px; border-bottom: 1px solid rgba(219, 221, 229, 0.6);">最近文章</div>
-
+    <div class="love-code-article-header">最近文章</div>
+    <el-card class="box-card" shadow="hover">
+      <div v-for="o in 4" :key="o" class="text item">
+        {{'列表内容1 ' + o }}
+      </div>
+    </el-card>
+    <el-card class="box-card" shadow="hover">
+      <div v-for="o in 4" :key="o" class="text item">
+        {{'列表内容1 ' + o }}
+      </div>
+    </el-card>
     <el-card class="box-card" shadow="hover">
       <div v-for="o in 4" :key="o" class="text item">
         {{'列表内容1 ' + o }}
@@ -17,7 +26,13 @@ export default {
 </script>
 
 <style scoped>
+  .love-code-article-header {
+    width : 100%;
+    height : 60px;
+    border-bottom: 1px solid rgba(219, 221, 229, 0.6);
+  }
   .el-card {
+    margin-top : 3%;
     cursor : pointer;
   }
 </style>
